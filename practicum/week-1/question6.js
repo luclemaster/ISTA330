05bdf5bcdd098d0580c27eebbff2dacd9bd73098
luclemaster <llemaster@email.arizona.edu>
2020-09-05 11:13:00 -0700

    added question 6

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -6,5 +6,28 @@ output: 3
 */
 
 var median = function(input) {
-
+    var sortList = [];
+    while(input.length > 0)
+    {
+        let i;
+        let min = 0;
+        for (i=0; i< input.length; i++)
+        { 
+            if (input[i] < input[min])
+            {
+                min = i;
+            }
+        }
+        let removed = input.splice(min, 1);
+        sortList += removed;
+    }
+    console.log(sortList);
+    if (sortList.length % 2 == 1)
+    {
+        return sortList[Math.floor(sortList.length / 2)];
+    }
+    else 
+    {
+        return (sortList[(sortList.length / 2)] + sortList[(sortList.length / 2) - 1]) / 2;
+    }
 };
