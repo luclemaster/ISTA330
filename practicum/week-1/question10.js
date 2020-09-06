aa332ac16d9c1ee5756ce18401f13335e0917710
luclemaster <llemaster@email.arizona.edu>
2020-09-06 15:06:29 -0700

    added q10

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -7,5 +7,20 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+    let nString = n.toString()
+    var max = parseInt(nString[0]);
+    var min = parseInt(nString[0]);
+    for (let i = 0; i < nString.length; i++)
+    {
+        if(parseInt(nString[i]) < min)
+        {
+            min = parseInt(nString[i]);
+        }
+        if(parseInt(nString[i]) > max)
+        {
+            max = min = parseInt(nString[i]);
+        }
+    }
+    return max - min;
 };
+console.log(maxMinusMin(472));
