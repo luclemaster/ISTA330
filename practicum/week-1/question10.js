5f5ff9e8016ecbe4311ed9a44cbe715801dcd74c
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-09-07 13:50:28 -0700

    Question 10

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -8,5 +8,15 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+    let max = 0;
+    let min = 9;
+    for (i = 0; i < n.length; i++) {
+        if (n[i] > max) {
+            max = n[i];
+        }
+        if (n[i] < min) {
+            min = n[i];
+        }
+    }
+    return (max - min);
 };
