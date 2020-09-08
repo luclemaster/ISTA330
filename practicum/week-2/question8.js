c72ee5823bd90b18103e1b24ac036daf99e73b43
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-09-07 19:15:49 -0700

    Question 8

--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -13,5 +13,27 @@ output: 3
 */
 
 var d_integer = function(input) {
-
+    let outputs = []
+    for (i = 0; i < input.length; i++) {
+        let number = 0
+        let current = input[i]
+        for (x = 0; x < input.length; x++) {
+            if (input[x] == current) {
+                number += 1
+            }
+        }
+        if (number = current) {
+            outputs.push(current)
+        }
+    }
+    if (outputs.length > 0) {
+        let max = 0
+        for (i = 0; i < outputs.length; i++) {
+            if (outputs[i] > max) {
+                max = outputs[i]
+            }
+        }
+        return max
+    }
+    return -1
 };
