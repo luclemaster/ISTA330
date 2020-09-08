ea8d750248e71e972a32d629ce6b81db84d999a5
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2020-09-07 22:43:14 -0700

    Missing =

--- practicum/week-2/question12.js
+++ practicum/week-2/question12.js
@@ -9,15 +9,15 @@ Given n, calculate F(n).
 */
 
 var F = function(n) {
-    if (n = 0){
+    if (n == 0){
         return 0
     }
     if (n == 1 || n == 2){
         return 1
     }
-    let nums = [0,1]
-    for (i = 2; i <= n; i++){
-        nums.push(nums[i-2] + nums[i-1])
+    let nums = [0,1];
+    for (i = 2; i < n + 1; i++){
+        nums.push(nums[i - 2] + nums[i-1])
     }
     return nums[n]
 };
