a7099f03a1a5bf2b320342c6cce22aefdcdfcaa8
luclemaster <llemaster@email.arizona.edu>
2020-09-08 01:17:28 -0700

    added q2

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -10,6 +10,22 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
-    
+         maxSum = 0;
+     for(let i = 0; i < input.length; i++)
+     {
+             let curSum = input[i];
+             if (curSum > maxSum)
+             {
+                     maxSum = curSum;
+             }
+             for(let j = i + 1; j < input.length; j++)
+             {
+                     curSum += input[j];
+                     if (curSum > maxSum)
+                     {
+                             maxSum = curSum;
+                     }
+             }
+     }
+    return maxSum;
  };
