64e4d1f0806272ae8c3c8954ff6543540a04e8fe
luclemaster <llemaster@email.arizona.edu>
2020-09-08 04:04:47 -0700

    Update question6.js

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -6,14 +6,14 @@ output: 3
 */
 
 var median = function(input) {
-    /*
+    
     if (input.length % 2 == 1)
     {
-        return input[Math.floor(sortList.length / 2)];
+        return input[Math.floor(input.length / 2)];
     }
     else 
     {
         return (input[(input.length / 2)] + input[(input.length / 2) - 1]) / 2;
     }
-    */
+    
 };
