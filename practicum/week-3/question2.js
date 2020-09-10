b8a3a5ab79539b69387c42d4c2a01a9b2fbfa825
luclemaster <llemaster@email.arizona.edu>
2020-09-10 10:42:52 -0700

    w3 q2

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -11,6 +11,24 @@ Example: input: [19,3,2,10,8,2,3,5]
           */
 
  var findDuplicates = function(input) {
-     
-    
+     var duplicateDict = {};
+     for(let i =0; i < input.length; i++)
+        if(!(input[i] in duplicateDict)) 
+        {
+            duplicateDict[input[i]] = 0;
+        }
+        else
+        {
+            duplicateDict[input[i]]++;
+        }
+    duplicateList = [];
+    for (let e in duplicateDict)
+    {
+        if(duplicateDict[e]> 0)
+        {
+            duplicateList.push(parseInt(e));
+        }
+    }
+    return duplicateList;
  };
+ console.log(findDuplicates([19,3,2,10,8,2,3,5]));
