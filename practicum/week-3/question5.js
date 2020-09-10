f42ed2d802f117fea8f469097b9f0d08ae876383
luclemaster <llemaster@email.arizona.edu>
2020-09-10 11:13:55 -0700

    w3 q5

--- practicum/week-3/question5.js
+++ practicum/week-3/question5.js
@@ -10,7 +10,19 @@ output: [24,12,8,6]
 */
 
 var productOfOthers = function(input) {
-
+    productList = [];
+    for(let i = 0; i < input.length; i++)
+    {
+        productList[i] = 1;
+        for(let j = 0; j < input.length; j++)
+        {
+            if(i != j)
+            {
+                productList[i] *= input[j];
+            }
+        }
+    }
+    return productList;
 };
-
+console.log(productOfOthers([1,2,3,4]));
 console.log('hi');
