3dd5a62df77cc0f42019a2bbdf63ce08738bfc83
luclemaster <llemaster@email.arizona.edu>
2020-09-17 10:13:09 -0700

    w3 q9

--- practicum/week-3/question9.js
+++ practicum/week-3/question9.js
@@ -11,5 +11,34 @@ output: 1
 */
 
 var minMoves = function(A) {
-
+    var orderList = [];
+    var minVal = A[0];
+    for(let i = 1; i < A.length; i++)
+    {
+        if(A[i] < minVal)
+        {
+            minVal = A[i];
+        }
+    }
+    orderList = [minVal];
+    var totalMoves = 0;
+    for(let i = 0; i < A.length; i++)
+    {
+        let largerVal = Math.pow(10,10);
+        for(let j = 0; j < A.length; j++)
+        {
+            if (A[j] > orderList[orderList.length-1] && A[j] < largerVal)
+            {
+                largerVal = A[j];
+            }
+        }
+        if (largerVal == Math.pow(10,10))
+        {
+            continue;
+        }
+        totalMoves += largerVal - orderList[orderList.length-1];
+        orderList.push(largerVal);
+    }
+    return totalMoves;
 };
+console.log(minMoves([1,2,2]));
